38b84bb3bf89ea8cd08dd10ec91715b052904912
DNSK <sslgh1024@naver.com>
2024-08-01 14:46:34 +0900

    화살 토글 기능 추가 R로 토글

--- Assets/#1 Scripts/#1 Player/ArrowController.cs
+++ Assets/#1 Scripts/#1 Player/ArrowController.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.InputSystem;
 
 public class ArrowController : MonoBehaviour
 {
@@ -9,26 +9,44 @@ public class ArrowController : MonoBehaviour
     public float followSpeed;
     public float rotationSpeed;
 
+    public bool CanControllArrow = false;
+    private Rigidbody2D rigidbody;
+
     public void Start()
     {
         Arrow.transform.gameObject.SetActive(true);
+        rigidbody = GetComponent<Rigidbody2D>();
     }
     
     public void Update()
     {
-        Vector3 mousePosition = Input.mousePosition;
-        mousePosition.z = UnityEngine.Camera.main.nearClipPlane; // 카메라와의 거리 설정
-        Vector3 worldPosition = UnityEngine.Camera.main.ScreenToWorldPoint(mousePosition);
+        if (CanControllArrow)
+        {
+            rigidbody.gravityScale = 0;
+            rigidbody.velocity = Vector3.zero;
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = UnityEngine.Camera.main.nearClipPlane; // 카메라와의 거리 설정
+            Vector3 worldPosition = UnityEngine.Camera.main.ScreenToWorldPoint(mousePosition);
 
-        // 화살표 마우스위치로 이동
-        Arrow.transform.position = Vector3.Lerp(Arrow.transform.position, new Vector3(worldPosition.x, worldPosition.y, 0), followSpeed * Time.deltaTime);
+            // 화살표 마우스위치로 이동
+            Arrow.transform.position = Vector3.Lerp(Arrow.transform.position, new Vector3(worldPosition.x, worldPosition.y, 0), followSpeed * Time.deltaTime);
 
-        // 방향 계산
-        Vector3 direction = worldPosition - Arrow.transform.position;
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f; // 필요하면 각도 추가
-        Quaternion targetRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            // 방향 계산
+            Vector3 direction = worldPosition - Arrow.transform.position;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f; // 필요하면 각도 추가
+            Quaternion targetRotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
-        // 화살표 회전 -> 마우스 방향으로
-        Arrow.transform.rotation = Quaternion.RotateTowards(Arrow.transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            // 화살표 회전 -> 마우스 방향으로
+            Arrow.transform.rotation = Quaternion.RotateTowards(Arrow.transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        }
+        else
+        {
+            rigidbody.gravityScale = 1;
+        }
+    }
+
+    public void OnArrow(InputValue value)
+    {
+        CanControllArrow = !CanControllArrow;
     }
 }
