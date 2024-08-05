656d2f5e7d15987f6f689d63eac6dc58eb7fda32
DNSK <sslgh1024@naver.com>
2024-08-05 18:10:57 +0900

    문 코드 추가함

--- Assets/#1 Scripts/#5 ObjectControl/Door.cs
+++ Assets/#1 Scripts/#5 ObjectControl/Door.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class Door : MonoBehaviour, ISignalReceive
 {
     [HideInInspector] public int SignalType;
+    [HideInInspector] public string DoorType;
+    private bool Signal = false;
     
     public void Start()
     {
@@ -14,5 +16,27 @@ public class Door : MonoBehaviour, ISignalReceive
     public void SignalChanged(bool signal)
     {
         Debug.Log((signal)?"문열림":"문닫힘");
+        Signal = signal;
+    }
+
+    private void Update()
+    {
+        if (DoorType == "UpDown")
+        {
+            if (Signal)
+            {
+                transform.position =
+                    Vector3.Slerp(transform.position, new Vector3(0, 4, 0), 10f*Time.deltaTime);
+            }
+            else
+            {
+                transform.position =
+                    Vector3.Slerp(transform.position, new Vector3(0, 0, 0), 10f*Time.deltaTime);
+            }
+        }
+        else
+        {
+            
+        }
     }
 }
--- Assets/Editor/DoorEditor.cs
+++ Assets/Editor/DoorEditor.cs
@@ -5,18 +5,21 @@ using UnityEditor;
 public class DoorEditor : Editor
 {
     private string[] signalOptions = new string[] { "0","1","2","3","4","5","6","7","8","9" };
+    private string[] doorTypeOptions = new string[] { "위아래", "회전" };
 
     public override void OnInspectorGUI()
     {
         Door script = (Door)target;
         
         int signalSelectedIndex = script.SignalType;
+        int doorTypeIndex = script.DoorType=="UpDown"?0:1;
         
         // 기본 인스펙터 그리기
         DrawDefaultInspector();
 
         // 드롭다운 리스트 추가
         signalSelectedIndex = EditorGUILayout.Popup("Select Signal", signalSelectedIndex, signalOptions);
+        doorTypeIndex = EditorGUILayout.Popup("Select Door Option", doorTypeIndex, doorTypeOptions);
         
         // 변경사항 적용
         if (GUI.changed)
@@ -24,5 +27,6 @@ public class DoorEditor : Editor
             EditorUtility.SetDirty(script);
         }
         script.SignalType = int.Parse(signalOptions[signalSelectedIndex]);
+        script.DoorType = doorTypeIndex == 0 ? "UpDown" : "Rotate";
     }
 }
