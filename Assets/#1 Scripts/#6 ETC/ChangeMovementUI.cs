888a3efd8e1c070b57e386fc9a5b65439839c014
이건희 <79892675+LOOPARAM@users.noreply.github.com>
2024-09-21 23:30:34 +0900

    스테이지마다 화살 추가 여부 UI수정

--- Assets/#1 Scripts/#6 ETC/ChangeMovementUI.cs
+++ Assets/#1 Scripts/#6 ETC/ChangeMovementUI.cs
@@ -36,5 +36,19 @@ public class ChangeMovementUI : MonoBehaviour
                 ArrowUI.localScale = Vector3.Slerp(ArrowUI.localScale, new Vector3(1, 1, 1), 10 * dt);
                 break;
         }
+
+        var headUILocalPosition = HeadUI.anchoredPosition3D;
+        var bodyUILocalPosition = BodyUI.anchoredPosition3D;
+
+        headUILocalPosition.x = (!GameManager.Instance.useArrow) ? -60 : -120;
+        bodyUILocalPosition.x = (!GameManager.Instance.useArrow) ? 60 : 0;
+
+        HeadUI.anchoredPosition = headUILocalPosition;
+        BodyUI.anchoredPosition = bodyUILocalPosition;
+
+        ArrowUI.gameObject.SetActive(GameManager.Instance.useArrow);
+
+        
+        
     }
 }
