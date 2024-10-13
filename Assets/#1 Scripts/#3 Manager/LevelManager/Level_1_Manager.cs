a69782a143724d537141a4184a54a165b8f3e2c7
이건희 <79892675+LOOPARAM@users.noreply.github.com>
2024-10-13 20:02:09 +0900

    레벨 코멘트 구현

--- Assets/#1 Scripts/#3 Manager/LevelManager/Level_1_Manager.cs
+++ Assets/#1 Scripts/#3 Manager/LevelManager/Level_1_Manager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class Level_1_Manager : MonoBehaviour
@@ -10,7 +11,9 @@ public class Level_1_Manager : MonoBehaviour
     public Transform body;
     public Transform arrow;
     public Transform cam;
-    
+
+    public GameObject[] Comments;
+    private int index = 0;
     
     public void Awake()
     {
@@ -20,5 +23,19 @@ public class Level_1_Manager : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
         GameManager.Instance.CheckChangeScene(other,1);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return) && index < 3)
+        {
+            Comments[index].SetActive(false);
+            index += 1;
+            if (index == 3)
+            {
+                return;
+            }
+            Comments[index].SetActive(true);
+        }
     }
 }
