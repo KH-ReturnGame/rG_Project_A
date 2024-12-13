0c7ccd89d568f745b073302c4047d8d1fdbe3047
DNSK <sslgh1024@naver.com>
2024-12-13 23:04:49 +0900

    화살, 몸 충돌 다시 켰으

--- Assets/#1 Scripts/#1 Player/BodyCollide.cs
+++ Assets/#1 Scripts/#1 Player/BodyCollide.cs
@@ -33,7 +33,8 @@ public class BodyCollide : MonoBehaviour
     {
         if ((other.CompareTag("ground") && name == "body_ground_check")||
             (other.CompareTag("Head") && name == "body_ground_check")||
-            (other.CompareTag("Door") && name == "body_ground_check"))
+            (other.CompareTag("Door") && name == "body_ground_check")||
+            (other.CompareTag("Arrow") && name == "body_ground_check"))
         {
             player.AddState(PlayerStats.BodyIsGround);
             _collideCount++;
@@ -132,7 +133,8 @@ public class BodyCollide : MonoBehaviour
         //땅에서 떨어졌다고 땅감지 콜라이더가 체크하면 땅감지 해제
         if ((other.CompareTag("ground")&&name == "body_ground_check")||
             (other.CompareTag("Head") && name == "body_ground_check")||
-            (other.CompareTag("Door") && name == "body_ground_check"))
+            (other.CompareTag("Door") && name == "body_ground_check")||
+            (other.CompareTag("Arrow") && name == "body_ground_check"))
         {
             _collideCount--;
             if (_collideCount == 0)
