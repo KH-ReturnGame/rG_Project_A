5670ca92c8030bb2fb517308a4f293a57dc393e9
DNSK <sslgh1024@naver.com>
2024-12-17 13:55:41 +0900

    드이어 ㅅㅂ 애님에ㅣ션 문제 고쳤다

--- Assets/#1 Scripts/#1 Player/body_anime.cs
+++ Assets/#1 Scripts/#1 Player/body_anime.cs
@@ -17,25 +17,25 @@ public class body_anime : MonoBehaviour
 
     void Update()
     {
-        // 애니메이터 상태 정보 가져오기
-        AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
-        //Debug.Log(stateInfo.normalizedTime);
-
-        // 특정 애니메이션이 끝났는지 확인 (이름은 애니메이션 상태 이름으로 대체)
-        if (!h && stateInfo.normalizedTime >= 0.95f)
-        {
-            // material을 기본으로 변경
-            spriteRenderer.material = material[0];
-            animator.SetBool("isGround", false);
-            h = true;
-        }
-        if (!k && stateInfo.normalizedTime >= 0.95f)
-        {
-            // material을 기본으로 변경
-            spriteRenderer.material = material[3];
-            animator.SetBool("isGround", false);
-            k = true;
-        }
+        // // 애니메이터 상태 정보 가져오기
+        // AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+        // Debug.Log(stateInfo.normalizedTime);
+        //
+        // // 특정 애니메이션이 끝났는지 확인 (이름은 애니메이션 상태 이름으로 대체)
+        // if (!h && stateInfo.normalizedTime >= 0.9f)
+        // {
+        //     // material을 기본으로 변경
+        //     spriteRenderer.material = material[0];
+        //     animator.SetBool("isGround", false);
+        //     h = true;
+        // }
+        // if (!k && stateInfo.normalizedTime >= 0.9f)
+        // {
+        //     // material을 기본으로 변경
+        //     spriteRenderer.material = material[3];
+        //     animator.SetBool("isGround", false);
+        //     k = true;
+        // }
     }
 
     // Glow 효과를 줄 때 호출
@@ -45,9 +45,24 @@ public class body_anime : MonoBehaviour
         h = false;
     }
 
+    public void endMat()
+    {
+        spriteRenderer.material = material[0];
+        animator.SetBool("isGround", false);
+        h = true;   
+    }
+
     public void startMat2()
     {
         spriteRenderer.material = material[2]; // Glow material 적용
         k = false;
+    }
+
+    public void endMat2()
+    {
+        // material을 기본으로 변경
+        spriteRenderer.material = material[3];
+        animator.SetBool("isGround", false);
+        k = true;
     }
 }
